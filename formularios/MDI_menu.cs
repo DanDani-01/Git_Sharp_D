ee9dd4be15d2f465efe2607c1ce29074b5bac798
DanDani-01 <daniel.rt.oliveira@gmail.com>
2026-03-09 12:28:58 -0300

    MDI_Adicionado

---
+++ formularios/MDI_menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Git_Sharp.formularios
+{
+    public partial class MDI_menu : Form
+    {
+        public MDI_menu()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comBotõesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
